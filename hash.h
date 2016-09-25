db15c0bd443964ba1b571737b76481415cffbfe7
GustavoSchwantz <gustavo_aso1994@hotmail.com>
2016-09-25 02:02:27 -0300

    hash table using doubly-linked list

---
+++ hash.h
@@ -0,0 +1,19 @@
+#include "linked-list.h"
+
+#define LOAD_FACTOR 2
+
+struct table {
+	int t_size;
+	int elements;
+	LIST *table;
+};
+
+typedef struct table TABLE;
+
+int h (int k, int n);
+void table_factory (TABLE *t, int n);
+void chained_hash_insert (TABLE *t, cel *x);
+cel *chained_hash_search (TABLE *t, int k);
+void chained_hash_delete (TABLE *t, cel *x);
+void rehash (TABLE *t);
+void destroy_table (LIST *table, int n);
